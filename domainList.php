35191cfcdc7b15eff0a73fb9b54a649fe7f6291a
killas96 <60001978+killas96@users.noreply.github.com>
2021-09-03 14:25:49 +0500

    Create domainList.php

---
+++ domainList.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+	'Москва' => array(
+		'genitive' => 'Москвы',
+        'dative' => 'Москве',
+	),
+	'Уфа' => array(
+		'genitive' => 'Уфы',
+        'dative' => 'Уфе',
+	),
+);
+
+?>
